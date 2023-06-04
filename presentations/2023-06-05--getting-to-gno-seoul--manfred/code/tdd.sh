d0d1cba391e0ecb0aae7439d70ebabbee0556bbd
Manfred Touron <94029+moul@users.noreply.github.com>
2023-06-04 20:43:37 +0200

    feat: add getting to gno slides (#15)

---
+++ presentations/2023-06-05--getting-to-gno-seoul--manfred/code/tdd.sh
@@ -0,0 +1,10 @@
+# install the "gno" CLI
+$> git clone git@github.com:gnolang/gno.git
+$> cd ./gno
+$> make install_gno
+
+# run test
+$> gno test --verbose .
+=== RUN   TestPackage
+--- PASS: TestPackage (0.01s)
+ok      ./.     0.50s
