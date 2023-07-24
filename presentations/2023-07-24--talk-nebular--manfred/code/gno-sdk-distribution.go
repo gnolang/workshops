79e89b9a98a1b4178120b82981b135020291e6dd
Manfred Touron <94029+moul@users.noreply.github.com>
2023-07-24 14:14:48 +0200

    chore: fixup
    Signed-off-by: Manfred Touron <94029+moul@users.noreply.github.com>

--- presentations/2023-07-24--talk-nebular--manfred/code/gno-sdk-distribution.go
+++ presentations/2023-07-24--talk-nebular--manfred/code/gno-sdk-distribution.go
@@ -1,12 +1,13 @@
-// baseapp: chain fees (gas) are sent to `sdk.GetRealm("r/system/rewards").Banker`
+// baseapp: chain fees (gas) are sent to the rewards' banker.
+gasDestination := sdk.GetRealm("r/system/rewards").Banker().Addr()
 
-// r/system/rewards: distribution logic implemented in contract,
-// querying other contracts, applying rules
+// r/system/rewards: Distribution logic implemented in the contract,
+// querying other contracts, applying rules.
 import "gno.land/r/system/validators"
 import "gno.land/r/gnoland/dao"
 func Distribute() {
-	// split
+	// Split rewards among recipients.
 }
 
-// baseapp: relevant chain event (double sig, etc) can be sent to the contract
+// Baseapp: Relevant chain events (double sig, etc.) can be sent to the contract.
 r.GetRealm("r/system/rewards").Send(abci.Event{})
